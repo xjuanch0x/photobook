92318fcf7ef108c5680f9095d11ca96ae5707e38
Juan David Torres <juandavidtorres@live.com>
2016-05-21 16:59:52 +0000

    Install paperclip Gem and add Post's photo field

--- Gemfile
+++ Gemfile
@@ -43,6 +43,10 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem 'foundation-rails', '~> 5.5', '>= 5.5.3.2'
+ 
 end
+
+gem 'paperclip', '~> 4.3', '>= 4.3.2'
+
+gem 'foundation-rails', '~> 5.5', '>= 5.5.3.2'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521161604) do
+ActiveRecord::Schema.define(version: 20160521165033) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
